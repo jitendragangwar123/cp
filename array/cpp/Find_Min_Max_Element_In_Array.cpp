474503dde8c826a2845ed7cc27f3fa0497b932a2
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2021-03-01 03:09:26 +0530

    Add_Comments

--- array/cpp/Find_Min_Max_Element_In_Array.cpp
+++ array/cpp/Find_Min_Max_Element_In_Array.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
 
+//Function to find the min and max value
 void findMinMaxElement(int arr[] , int n){
-   int max = arr[0], min = arr[0];
-   for(int i = 1; i < n; i++){
+     int max = arr[0], min = arr[0];
+     
+     for(int i = 1; i < n; i++){
       //find the maximum value 
-      if(max < arr[i])
+     if(max < arr[i])
          max = arr[i];
       //find the minimum value   
-      if(min > arr[i])
+     if(min > arr[i])
          min = arr[i];
    }
    cout<<"Maximum value in the Array= "<<max<<"\n";
@@ -17,19 +19,17 @@ void findMinMaxElement(int arr[] , int n){
 int main(){
     int t;
     cout<<"Enter the test case: "<<endl;
-	cin>>t;
-	while(t--){
-
-	    int n;
-        cout<<"Enter the Size of an Array: "<<endl;
-	    cin>>n;
-	    int arr[n];
-        cout << "Enter the element in the Array: "<<endl; 
-	        for(int i=0;i<n;i++){
-	          cin>>arr[i];
-	        }
-   
-    findMinMaxElement(arr, n);
+    cin>>t;
+    while(t--){
+         int n;
+         cout<<"Enter the Size of an Array: "<<endl;
+	 cin>>n;
+	 int arr[n];
+         cout << "Enter the element in the Array: "<<endl; 
+	 for(int i=0;i<n;i++){
+	     cin>>arr[i];
+	  }
+     findMinMaxElement(arr, n);
     }
    return 0;
 }
